b4282018414a7d0b666e2dac162e2d4af970db1f
priscillaWare <146377671+priscillaWare@users.noreply.github.com>
2025-02-09 16:19:53 -0700

    fixed some syntax and started spork()

--- phase1a.c
+++ phase1a.c
@@ -5,6 +5,9 @@
 
 static Process process_table[MAXPROC];
 
+Process* running_procces = NULL;
+int next_pid = 1;
+
 extern int testcase_main(void *arg);
 
 void phase1_init(){
@@ -14,16 +17,17 @@ void phase1_init(){
     USLOSS_PsrSet(psr & ~USLOSS_PSR_CURRENT_INT);
 
     for (int i = 0; i < MAXPROC; i++) {
-        process_table[i].pid = -1
+        process_table[i].pid = -1;
     }
-    process_table[0].pid = 1
+    process_table[0].pid = 1;
+    next_pid++;
     process_table[0].priority = 6;
-    strncpy(processTable[0].name, "init", MAXNAME);
-    process_table[0].status = 0
+    strncpy(process_table[0].name, "init", MAXNAME);
+    process_table[0].status = 0;
     
     //start testcase main
     int testcase_pid = spork("testcase_main", testcase_main, NULL, USLOSS_MIN_STACK, 3);
-    USLOSS_ContextSwitch(USLOSS_Context *old, USLOSS_Context *new)
+    USLOSS_ContextSwitch(USLOSS_Context *old, USLOSS_Context *new);     // still need to figure ou how to properly use this function call...
     Process *testcaseProcess = &process_table[testcase_pid % MAXPROC];
     Process *initProcess = &process_table[0];
 
@@ -49,9 +53,38 @@ void wrapper(){
     // wrapper for a function... gets info about a process
 }
 
-void spork(char *name, int (*startFunc)(void*), void *arg, int stackSize,
+int spork(char *name, int (*startFunc)(void*), void *arg, int stackSize,
 int priority){
-    // creates a new process, which is a child of the current process
+
+    // adds a new process to the table
+    int psr = USLOSS_PsrGet();
+    USLOSS_PsrSet(psr & ~USLOSS_PSR_CURRENT_INT);
+
+    if (stackSize < USLOSS_MIN_STACK){
+        return -2;
+    }
+
+    // puts the new process in the table
+    int slot = next_pid % MAXPROC;
+    if (process_table[slot].pid == -1 || startFunc == NULL || name == NULL || strlen(name) > MAXNAME){
+        return -1;
+    }
+    process_table[slot].pid = next_pid;
+    next_pid++;
+    process_table[0].priority = 6;
+    strncpy(processTable[0].name, name, MAXNAME);
+    process_table[0].status = 0;
+
+    // adds the process to the linked list of children processes 
+    Process* cur = running_process->children;
+    while (cur != NULL){
+        cur = cur->next;
+    }
+    cur = &process_table[slot];
+
+    USLOSS_PsrSet(psr);
+
+    return process_table[slot].pid;
 }
 
 int join(int *status){
