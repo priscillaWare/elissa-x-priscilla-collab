f5586326277dd16751eddef3cda8ff8c99faeda9
priscillaWare <146377671+priscillaWare@users.noreply.github.com>
2025-02-11 18:19:35 -0700

    quit_phase1a, spork, join

--- phase1a.c
+++ phase1a.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "phase1.h"
 #include "usloss.h"
 
 static Process process_table[MAXPROC];
 
-Process* running_procces = NULL;
+Process *running_process = NULL;
 int next_pid = 1;
 
 extern int testcase_main(void *arg);
@@ -18,16 +19,21 @@ void phase1_init(){
 
     for (int i = 0; i < MAXPROC; i++) {
         process_table[i].pid = -1;
+        process_table[i].status = 0;
+        process_table[i].children = NULL;
+        process_table[i].next = NULL;
     }
     process_table[0].pid = 1;
     next_pid++;
     process_table[0].priority = 6;
     strncpy(process_table[0].name, "init", MAXNAME);
     process_table[0].status = 0;
+
+    running_process = &process_table[0];
     
     //start testcase main
     int testcase_pid = spork("testcase_main", testcase_main, NULL, USLOSS_MIN_STACK, 3);
-    USLOSS_ContextSwitch(USLOSS_Context *old, USLOSS_Context *new);     // still need to figure ou how to properly use this function call...
+    USLOSS_ContextSwitch(&old_ctx, &new_ctx);     // still need to figure ou how to properly use this function call...
     Process *testcaseProcess = &process_table[testcase_pid % MAXPROC];
     Process *initProcess = &process_table[0];
 
@@ -66,14 +72,18 @@ int priority){
 
     // puts the new process in the table
     int slot = next_pid % MAXPROC;
-    if (process_table[slot].pid == -1 || startFunc == NULL || name == NULL || strlen(name) > MAXNAME){
+    if (process_table[slot].pid != -1 || startFunc == NULL || name == NULL || strlen(name) > MAXNAME){
+        USLOSS_PsrSet(psr);
         return -1;
     }
     process_table[slot].pid = next_pid;
     next_pid++;
-    process_table[0].priority = 6;
-    strncpy(processTable[0].name, name, MAXNAME);
+    process_table[slot].priority = priority;  // not super sure how to assign priority rn
+    strncpy(process_table[0].name, name, MAXNAME);
     process_table[0].status = 0;
+    process_table[slot].children = NULL;
+    process_table[slot].next = running_process->children;
+    running_process->children = &process_table[slot];
 
     // adds the process to the linked list of children processes 
     Process* cur = running_process->children;
@@ -91,13 +101,34 @@ int join(int *status){
     // blocks the current process until one of its children has terminated; 
     // it then delivers the “status” of the child (the parameter 
     // that the child passed to quit()) back to the parent
-    return 0;
+    if (status == NULL){
+        return -3;
+    }
+    Process *child = running_process->children;
+    while (child != NULL){
+        if (child->status == -1){
+            *status = child->status;
+            child->pid= -1;
+            return child->pid;
+        }
+        child = child->next;
+    }
+    return -2;
 }
 
 void quit(int status){
     // NEVER RETURNS. terminates the current process with a 'status' value
     // If the parent of the proccess is already waiting in a join(), the
     // parent will be awoken. 
+}
+
+void quit_phase_1a(int status, int switchToPid) {
+    running_process->status = -1;
+    for (int i = 0; i < MAXPROC; i++){
+        if (process_table[i].pid == switchToPid){
+            USLOSS_ContextSwitch(running_process.context, process_table[i].context);
+        }
+    }
 }
 
 void zap(int pid){
@@ -116,6 +147,11 @@ void dumpProcesses(){
 
 void blockMe(){
     // SKIP
+}
+
+int unblockProc(int pid){
+    // SKIP
+    return 0;
 }
 
 int unblockProc(int pid){
