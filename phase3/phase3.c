a95f435dbbdbb25202fb9b1148e93941432ad3a7
priscillaWare <146377671+priscillaWare@users.noreply.github.com>
2025-04-03 00:46:52 -0700

    tests 00 - 07

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -4,6 +4,11 @@
 #include <string.h>
 
 
+int sems[MAXSEMS];
+int mbox_lock_id;
+int queued;
+
+
 void sys_terminate(USLOSS_Sysargs *args) {
     int dummy = 0;
     int *status = &dummy;
@@ -50,10 +55,73 @@ void sys_wait(USLOSS_Sysargs *args) {
       }
 }
 
+void sys_semcreate(USLOSS_Sysargs *args) {
+   int index = -1;
+   for (int i = 0; i < MAXSEMS; i++) {
+     if (sems[i] == -1){
+       index = i;
+       break;
+     }
+   }
+   if (index == -1){
+     args->arg1 = 0;
+     args->arg4 = -1;
+     }
+   else {
+     sems[index] = args->arg1;
+     args->arg1 = index;
+     args->arg4 = 0;
+   }
+}
+
+void sys_semp(USLOSS_Sysargs *args) {
+    int index = args->arg1;
+    if (sems[index] == 0) {
+      queued++;
+      MboxSend(mbox_lock_id, NULL, 0);
+    }
+    sems[index]--;
+    if (index >= 0 && index < MAXSEMS) {
+      args->arg4 = 0;
+    }
+    else {
+      args->arg4 = 0;
+      }
+}
+
+void sys_semv(USLOSS_Sysargs *args) {
+    int index = args->arg1;
+    sems[index]++;
+    if (index >= 0 && index < MAXSEMS) {
+      args->arg4 = 0;
+    }
+    else {
+      args->arg4 = 0;
+      }
+    if (queued > 0) {
+      queued--;
+      MboxRecv(mbox_lock_id, NULL, 0);
+      }
+}
+
+void sys_getpid(USLOSS_Sysargs *args) {
+  args->arg1 = getpid();
+}
+
 void phase3_init(void){
+    for (int i = 0; i < MAXSEMS; i++) {
+      sems[i] = -1;
+    }
+    queued = 0;
+    mbox_lock_id = MboxCreate(0,0);
+
     systemCallVec[3] = sys_spawn;
     systemCallVec[4] = sys_wait;
     systemCallVec[5] = sys_terminate;
+    systemCallVec[16] = sys_semcreate;
+    systemCallVec[17] = sys_semp;
+    systemCallVec[18] = sys_semv;
+    systemCallVec[22] = sys_getpid;
   }
 
 
@@ -67,6 +135,11 @@ int kernSemCreate(int value, int *semaphore) {
 
 
 int kernSemP(int semaphore) {
+  return 0;
+  }
+
+
+int kernSemV(int semaphore){
   return 0;
   }
 
