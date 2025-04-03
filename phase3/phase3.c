746cd1a086ec8d344199750818adcbc94e799f25
priscillaWare <146377671+priscillaWare@users.noreply.github.com>
2025-04-02 20:17:17 -0700

    still need to implement trampoline

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -4,8 +4,55 @@
 #include <string.h>
 
 
-void phase3_init(void){
+void sys_terminate(USLOSS_Sysargs *args) {
+    int dummy = 0;
+    int *status = &dummy;
+    int retval = join(status);
+    while (retval != -2 && retval != -3) {
+        retval = join(status);
+    }
+    int outstatus = args->arg1;
+    quit(outstatus);
+}
 
+void trampoline(){
+  int result = USLOSS_PsrSet(current | USLOSS_PSR_CURRENT_INT);
+     if (result != USLOSS_DEV_OK) {
+         USLOSS_Halt(1);
+     }
+     
+  }
+
+void sys_spawn(USLOSS_Sysargs *args) {
+    int stacksize = (int)(long)args->arg3;
+    int priority = (int)(long)args->arg4;
+    printf("calling spork\n");
+    int retval = spork((char *)args->arg5, (int (*)(void*))args->arg1, args->arg2, stacksize, priority);
+    printf("spork returned %d\n", retval);
+
+    args->arg1 = retval;
+    args->arg4 = 0;
+}
+
+void sys_wait(USLOSS_Sysargs *args) {
+    int dummy = 0;
+    int *status = &dummy;
+    int retval = join(status);
+    printf("result = %d\n", retval);
+    args->arg2 = status;
+    args->arg1 = retval;
+    if (retval == -2) {
+      args->arg4 = -2;
+      }
+    else {
+      args->arg4 = 0;
+      }
+}
+
+void phase3_init(void){
+    systemCallVec[3] = sys_spawn;
+    systemCallVec[4] = sys_wait;
+    systemCallVec[5] = sys_terminate;
   }
 
 
@@ -13,64 +60,16 @@ void phase3_start_service_processes(void){
 
   }
 
-
-int Spawn(char *name, int (*func)(void *), void *arg, int stack size, int priority, int *pid) {
-  return 0;
-  }
-
-
-int Wait(int *pid, int *status) {
-  return 0;
-  }
-
-
-void Terminate(int status) {
-
-  }
-
-
-int SemCreate(int value, int *semaphore) {
-  return 0;
-  }
-
-
-int SemP(int semaphore) {
-  return 0;
-  }
-
-
-int SemV(int semaphore) {
-  return 0;
-  }
-
-
 int kernSemCreate(int value, int *semaphore) {
   return 0;
   }
-  
-  
+
+
 int kernSemP(int semaphore) {
   return 0;
   }
-  
-  
+
+
 int kernSemV(int semaphore){
   return 0;
   }
-
-
-void GetTimeofDay(int *tod) {
-  
-  }
-
-
-void GetPID(int *pid){
-
-  }
-
-
-void DumpProcesses(){
-
-  }
-
-
