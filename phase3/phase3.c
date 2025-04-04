8836e56d921a721f6be3842890a4f8b3df5ff240
priscillaWare <146377671+priscillaWare@users.noreply.github.com>
2025-04-03 18:17:51 -0700

    almost done

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int sems[MAXSEMS];
-int mbox_lock_id;
-int queued;
-void *argument;
+int sems[MAXSEMS];    // array of semaphores
+int mbox_lock_id;     // id of the mailbox used to block processes
+int queued;           // number of processes blocked by a mailbox
 
 
+// sys_terminate: Terminates the current process, with the status specified. This function never
+// returns. Calls join() over and over until it returns -2, then calls quit.
 void sys_terminate(USLOSS_Sysargs *args) {
     int dummy = 0;
     int *status = &dummy;
@@ -20,6 +21,9 @@ void sys_terminate(USLOSS_Sysargs *args) {
     quit(outstatus);
 }
 
+
+// trampoline: a wrapper for the user function. disables kernel mode, then
+// calls the user function with the specified arguments
 void trampoline(void *arg) {
     int result = USLOSS_PsrSet(USLOSS_PSR_CURRENT_INT);
     if (result != USLOSS_DEV_OK) {
@@ -37,6 +41,9 @@ void trampoline(void *arg) {
     Terminate(ret);  // Pass the return value to Terminate
 }
 
+
+// sys_spawn: creates a new user mode process. calls spork() and returns the
+// pid of the created process through args->arg1
 void sys_spawn(USLOSS_Sysargs *args) {
     int stacksize = (int)(long)args->arg3;
     int priority = (int)(long)args->arg4;
@@ -50,6 +57,7 @@ void sys_spawn(USLOSS_Sysargs *args) {
     args->arg4 = (void*)0;
 }
 
+// sys_wait: Calls join(), and returns the PID and status that join() provided.
 void sys_wait(USLOSS_Sysargs *args) {
     int dummy = 0;
     int *status = &dummy;
@@ -64,6 +72,10 @@ void sys_wait(USLOSS_Sysargs *args) {
       }
 }
 
+// sys_semcreate: creates a new semaphore and returns the semaphore ID, for use in later function calls.
+// initializes value to store in the semaphore to args-arg1. returns -1 if the initial value is negative
+// or if no semaphores are available.
+Otherwise returns 0.
 void sys_semcreate(USLOSS_Sysargs *args) {
    int index = -1;
    for (int i = 0; i < MAXSEMS; i++) {
@@ -83,6 +95,8 @@ void sys_semcreate(USLOSS_Sysargs *args) {
    }
 }
 
+// sys_semp: executes the P() operation, which decrements the value of the semaphore by 1, unless
+// the value is 0 in which case the process blocks via zero slot mailbox.
 void sys_semp(USLOSS_Sysargs *args) {
     int index = args->arg1;
     if (sems[index] == 0) {
@@ -98,6 +112,8 @@ void sys_semp(USLOSS_Sysargs *args) {
       }
 }
 
+// sys_semv: executes the V() operation, which increments the value of the semaphore by 1, and
+// wakes up any process blocked on P() via zero slot mailbox.
 void sys_semv(USLOSS_Sysargs *args) {
     int index = args->arg1;
     sems[index]++;
@@ -113,11 +129,13 @@ void sys_semv(USLOSS_Sysargs *args) {
       }
 }
 
+// sys_gettime: returns time of day via out pointer, arg1
 void sys_gettime(USLOSS_Sysargs *args) {
     args->arg1 = currentTime();
 
 }
 
+// sys_getpid: returns the pid via out pointer, arg1
 void sys_getpid(USLOSS_Sysargs *args) {
   args->arg1 = getpid();
 }
@@ -144,13 +162,60 @@ void phase3_start_service_processes(void){
 
   }
 
+// kernSemCreate: creates a new semaphore and returns the semaphore ID, for use in later function calls.
+// initializes value to store in the semaphore to args-arg1. returns -1 if the initial value is negative
+// or if no semaphores are available. Must be called from kernel mode.
 int kernSemCreate(int value, int *semaphore) {
-  return 0;
+  int index = -1;
+   for (int i = 0; i < MAXSEMS; i++) {
+     if (sems[i] == -1){
+       index = i;
+       break;
+     }
+   }
+   if (index == -1){
+     args->arg1 = 0;
+     args->arg4 = -1;
+     }
+   else {
+     sems[index] = args->arg1;
+     args->arg1 = index;
+     args->arg4 = 0;
+   }
   }
 
-
+// kernSemP: executes the P() operation, which decrements the value of the semaphore by 1, unless
+// the value is 0 in which case the process blocks via zero slot mailbox. Must be called in kernel mode.
 int kernSemP(int semaphore) {
-  return 0;
+    int index = args->arg1;
+    if (sems[index] == 0) {
+      queued++;
+      MboxSend(mbox_lock_id, NULL, 0);
+    }
+    sems[index]--;
+    if (index >= 0 && index < MAXSEMS) {
+      args->arg4 = 0;
+    }
+    else {
+      args->arg4 = 0;
+      }
+  }
+
+// kernSemV: executes the V() operation, which increments the value of the semaphore by 1, and
+// wakes up any process blocked on P() via zero slot mailbox. Must be called in kernel mode.
+int kernSemV(int semaphore){
+    int index = args->arg1;
+    sems[index]++;
+    if (index >= 0 && index < MAXSEMS) {
+      args->arg4 = 0;
+    }
+    else {
+      args->arg4 = 0;
+      }
+    if (queued > 0) {
+      queued--;
+      MboxRecv(mbox_lock_id, NULL, 0);
+      }
   }
 
 
